c984f8ea53098dd387e4783586ac13931730335e
Paul Ripmeester <18252621+HisEvilness@users.noreply.github.com>
2025-04-05 12:05:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ def calculate_modifier(exp, moral, logi): return exp * morale_scaling(moral) * l
 
 def calculate_casualties_range(base_rate, modifier, duration, ew_enemy, med, cmd, moral, logi):
     results, total = {}, {}
-    decay_strength = 0.0003 + 0.0001 * math.tanh(duration / 1000)
+    decay_strength = 0.00035 + 0.00012 * math.tanh(duration / 800)
     decay_curve_factor = 1 + decay_strength * duration * (1 - morale_scaling(moral)) * logistic_scaling(logi) * commander_scaling(cmd, duration)
     for system, share in weapons.items():
         logi_factor = logistic_scaling(logi)
